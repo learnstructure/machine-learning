1f01de8dc472ac4729cb35286ff39a9603bd6c16
abinash <abinashmandal33486@gmail.com>
2024-10-14 20:57:09 +0545

    SVR and LR also added

--- app.py
+++ app.py
@@ -6,16 +6,26 @@ import numpy as np
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 
-# Load your trained model
-model = joblib.load("GBR_tuned_Combined_V1.pkl")
+# Load your trained models
+models = {
+    "gbr": joblib.load("GBR_tuned_Combined_V1.pkl"),
+    "svr": joblib.load("best_model_svr.pkl"),
+    "linear": joblib.load("linear_model.pkl"),
+}
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.json
     features = np.array([data["features"]])  # Extract features from request
-    prediction = model.predict(features)
-    return jsonify({"prediction": prediction.tolist()})
+
+    # Get predictions from all models
+    predictions = {
+        model_name: model.predict(features).tolist()
+        for model_name, model in models.items()
+    }
+
+    return jsonify(predictions)
 
 
 if __name__ == "__main__":
